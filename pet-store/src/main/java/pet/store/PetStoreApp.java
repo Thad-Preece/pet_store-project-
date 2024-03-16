a7db23379dfbd6679b37340aa455f0c5a892a3cf
Thaddeus Preece <thadman1998@gmail.com>
2024-03-16 14:05:39 -0600

    Initial commit

---
+++ pet-store/src/main/java/pet/store/PetStoreApp.java
@@ -0,0 +1,13 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PetStoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApp.class, args);
+	}
+}
